8ed18321d700ff3b4423872d5e3673d7b45d2b0c
ice <smalltides@gmail.com>
2017-09-07 14:39:31 +0800

    add saveToFile func to save cards data to file

--- main.go
+++ main.go
@@ -2,8 +2,9 @@ package main
 
 func main() {
 	cards := newDeck()
-	hand, remainingCards := deal(cards, 5)
+	// hand, remainingCards := deal(cards, 5)
+	// hand.print()
+	// remainingCards.print()
 
-	hand.print()
-	remainingCards.print()
+	cards.saveToFile("my_cards")
 }
