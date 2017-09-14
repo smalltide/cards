8a8907ef40a5f876c8ff55914a813fdda982b36c
ice <smalltides@gmail.com>
2017-09-14 11:59:43 +0800

    use go routine and channel to check site status concurrency

--- channels/main.go
+++ channels/main.go
@@ -14,17 +14,29 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
+	}
+
+	// for s := range c {
+	// 	fmt.Println(s)
+	// }
+
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<-c)
 	}
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "might be down!"
 		return
 	}
 
 	fmt.Println(link, "is up!")
+	c <- "is up!"
 }
