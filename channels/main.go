a8a87e124c5aaeb7771904c98401cd1e6e72d890
ice <smalltides@gmail.com>
2017-09-14 13:18:03 +0800

    repeat try website

--- channels/main.go
+++ channels/main.go
@@ -20,12 +20,8 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	// for s := range c {
-	// 	fmt.Println(s)
-	// }
-
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	for {
+		go checkLink(<-c, c)
 	}
 }
 
@@ -33,10 +29,10 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "might be down!"
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
-	c <- "is up!"
+	c <- link
 }
